de86f244a1e8d2e41578c634d5287aae6f824d73
Oronoque <110566130+Oronoque@users.noreply.github.com>
2023-03-02 17:32:44 -0500

    adjusting picture addresses in contact and about

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,11 +1,11 @@
 import React from "react";
-// import profilePicture from "../../../static/assets/images/bio/middle-distance-rockstar.jpg";
+import profilePicture from "../../../static/assets/images/contact/leather-chair-guitar.jpg";
 
 export default function() {
   return (
   <div className="content-page-wrapper">
   <div className="left-column" style={{
-    // background: "url(" + profilePicture + ") no-repeat",
+    background: "url(" + profilePicture + ") no-repeat",
     backgroundSize: "cover",
     backgroundPosition: "center"
   }} 
